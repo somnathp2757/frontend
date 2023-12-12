33744e920918f5f6bc64daa64b76410e689fe4f5
somnathp2757 <139745338+somnathp2757@users.noreply.github.com>
2023-12-12 18:43:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hadiya_products_admin';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+	this.http.get('http://http://35.172.211.211:3000').subscribe((data: any)  => {
+  	console.log(data);
+	});
+  }
 }
