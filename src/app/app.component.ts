3a642f21f595858a34e6ccec8f6ba8c1af57f5a3
somnathp2757 <139745338+somnathp2757@users.noreply.github.com>
2023-12-14 12:56:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,3 +17,25 @@ export class AppComponent implements OnInit {
 	});
   }
 }
+
+/*  code for load balance URL
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'hadiya_products_admin';
+
+  constructor(private http: HttpClient) {}
+
+ ngOnInit() {
+        this.http.get('http://hadiya-backend-lb-74188492.us-east-1.elb.amazonaws.com').subscribe((data: any)  => {
+        console.log(data);
+        });
+  }
+} */
+
